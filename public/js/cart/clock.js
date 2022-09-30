d783b10fa02bf92c610f7d1050d8935d532ae200
Michael <michaeljoseph6950@gmail.com>
2022-09-30 21:40:55 +0300

    ch

--- public/js/cart/clock.js
+++ public/js/cart/clock.js
@@ -24,7 +24,7 @@ function move(){
                         if(seconds < 10){
                             seconds = '0'+seconds
                         }
-                        elemj.style.width = (width/9) + "%";
+                        elemj.style.width = (width/6) + "%";
                         document.getElementById('escoz').innerHTML = `Time left: ${minutes}:${seconds}`;
                     } 
                     else if( width <= 600) {
@@ -35,7 +35,7 @@ function move(){
                         if(seconds < 10){
                             seconds = '0'+seconds
                         }
-                        elemj.style.width = (width/9) + "%";
+                        elemj.style.width = (width/6) + "%";
                         document.getElementById('escoz').innerHTML = `Time left: ${minutes}:${seconds}`;
                     } 
                     else {
@@ -45,13 +45,13 @@ function move(){
                         if(seconds < 10){
                             seconds = '0'+seconds
                         }
-                        elemj.style.width = (width/9) + "%";
+                        elemj.style.width = (width/6) + "%";
                         document.getElementById('escoz').innerHTML = `Time left: ${minutes}:${seconds}`;
                     }
                 }
             } else if(!localStorage.getItem('loadedtime')) {
                 var elemj = document.getElementById('pablos');
-                localStorage.setItem('time-left',900)
+                localStorage.setItem('time-left',600)
                 var width = localStorage.getItem('time-left');
                 var id = setInterval(frame, 1000);
                 function frame(){
@@ -71,7 +71,7 @@ function move(){
                         if(seconds < 10){
                             seconds = '0'+seconds
                         }
-                        elemj.style.width = (width/9) + "%";
+                        elemj.style.width = (width/6) + "%";
                         document.getElementById('escoz').innerHTML = `Time left: ${minutes}:${seconds}`;
                     } 
                     else if( width <= 600) {
@@ -82,7 +82,7 @@ function move(){
                         if(seconds < 10){
                             seconds = '0'+seconds
                         }
-                        elemj.style.width = (width/9) + "%";
+                        elemj.style.width = (width/6) + "%";
                         document.getElementById('escoz').innerHTML = `Time left: ${minutes}:${seconds}`;
                     } 
                     else {
@@ -92,7 +92,7 @@ function move(){
                         if(seconds < 10){
                             seconds = '0'+seconds
                         }
-                        elemj.style.width = (width/9) + "%";
+                        elemj.style.width = (width/6) + "%";
                         document.getElementById('escoz').innerHTML = `Time left: ${minutes}:${seconds}`;
                     }
                 }
